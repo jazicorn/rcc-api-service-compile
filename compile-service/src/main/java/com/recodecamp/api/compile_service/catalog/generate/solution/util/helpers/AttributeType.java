860f36e34d24a6e394d874b55447eb1d67f73b8b
jazicorn <40859840+jazicorn@users.noreply.github.com>
2024-08-10 09:18:50 -0600

    created attribute types

---
+++ compile-service/src/main/java/com/recodecamp/api/compile_service/catalog/generate/solution/util/helpers/AttributeType.java
@@ -0,0 +1,10 @@
+package com.recodecamp.api.compile_service.catalog.generate.solution.util.helpers;
+
+public enum AttributeType {
+    DECLAREATTRIBUTE,
+    DECLAREATTRIBUTEVALUE,
+    PARAMETER,
+    INITIALVALUE,
+    THISINITIALVALUE,
+    THISINITIALVALUENOPARAMETER
+}
