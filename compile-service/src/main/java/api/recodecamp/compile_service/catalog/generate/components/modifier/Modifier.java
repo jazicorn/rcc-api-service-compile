b6ce03f1f1f096477bf61b523f8c994e78d275df
jazicorn <40859840+jazicorn@users.noreply.github.com>
2024-09-04 21:51:48 -0400

    updated modifier class

--- compile-service/src/main/java/api/recodecamp/compile_service/catalog/generate/components/modifier/Modifier.java
+++ compile-service/src/main/java/api/recodecamp/compile_service/catalog/generate/components/modifier/Modifier.java
@@ -4,21 +4,23 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import api.recodecamp.compile_service.catalog.generate.components.modifier.helpers.AccessModifier;
 import api.recodecamp.compile_service.catalog.generate.components.modifier.helpers.NonAccessModifier;
 import api.recodecamp.compile_service.catalog.generate.components.modifier.helpers.NonPrimitiveDataTypeModifier;
 import api.recodecamp.compile_service.catalog.generate.components.modifier.helpers.PrimitiveDataTypeModifier;
+
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class Modifier {
+public abstract class Modifier<T,K> {
 
     public AccessModifier accessModifier;
     public NonAccessModifier nonAccessModifier;
@@ -26,33 +28,37 @@ public abstract class Modifier {
     public NonPrimitiveDataTypeModifier nonPrimitiveDataTypeModifier;
     public String objectModifier;
 
-    @Override
-    public String toString() {
-
-        List<Field> getModifiers = Arrays.asList(Modifier.class.getDeclaredFields());
-
-        if (getModifiers.isEmpty() || getModifiers.containsAll(null))  {
-            return "";
-        } else {
-
-            // convert attributes to string
-            String modifiers = String.format("%s %s %s %s %s", accessModifier, nonAccessModifier,
-                    primitiveDataTypeModifier, nonPrimitiveDataTypeModifier, objectModifier);
-
-            // convert string to arr and remove null
-            String[] arr = Arrays.stream(modifiers
-                    .split(" "))
-                    .filter(Objects::nonNull)
-                    .toArray(String[]::new);
-
-            return String.join(" ", arr);
-        }
-    }
-
-    public Boolean isEmpty() {
-        return toString().trim().isEmpty();
+    public List<Field> modifiers() {
+        return Arrays.asList(Modifier.class.getDeclaredFields());
     };
 
-    public abstract void isModifierValid();
+    public Boolean isEmpty() {     
+        return (modifiers().isEmpty() || modifiers().containsAll(null));
+    };
+    
+    public String modifier() {
+        // convert attributes to string
+        String modifiers = String.format("%s %s %s %s %s", accessModifier, nonAccessModifier,
+                primitiveDataTypeModifier, nonPrimitiveDataTypeModifier, objectModifier);
 
+        // convert string to arr and remove null
+        String[] arr = Arrays.stream(modifiers
+                .split(" "))
+                .filter(Objects::nonNull)
+                .toArray(String[]::new);
+
+        return Optional.ofNullable(String.join(" ", arr)) == null ? "" : String.join(" ", arr);
+    };
+    
+    @Override
+    public String toString() {
+        String modifier = this.isEmpty() ? "" : modifier();
+        return modifier;
+    };
+
+    public abstract Boolean isValid();
+
+    public abstract Optional<T> accessType();
+
+    public abstract Optional<K> dataType();
 }
