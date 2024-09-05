c15d71c8c76f6ef6fc8fc0bc0369f198e58d0c10
jazicorn <40859840+jazicorn@users.noreply.github.com>
2024-09-04 21:53:58 -0400

    deleted AttributeModifier record

--- compile-service/src/main/java/api/recodecamp/compile_service/catalog/generate/components/attributes/ObjectAttribute.java
+++
@@ -1,8 +0,0 @@
-package api.recodecamp.compile_service.catalog.generate.components.attributes;
-
-public record ObjectAttribute(
-    AttributeType attributeType,
-    String attributeName,
-    String attributeValue
-) {
-};
