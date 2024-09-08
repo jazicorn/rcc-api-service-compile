f3a8c46cf7b42c7636641fa7676a830e695b0240
jazicorn <40859840+jazicorn@users.noreply.github.com>
2024-09-08 12:11:51 -0400

    update modifier class

--- src/main/java/api/recodecamp/compile_service/generate/components/modifier/Modifier.java
+++ src/main/java/api/recodecamp/compile_service/generate/components/modifier/Modifier.java
@@ -4,69 +4,101 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import api.recodecamp.compile_service.generate.components.modifier.helpers.AccessModifier;
 import api.recodecamp.compile_service.generate.components.modifier.helpers.NonAccessModifier;
-import api.recodecamp.compile_service.generate.components.modifier.helpers.NonPrimitiveDataTypeModifier;
 import api.recodecamp.compile_service.generate.components.modifier.helpers.PrimitiveDataTypeModifier;
 
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+
+/**
+* Modifier class generates modifier for objects
+* 
+* @author Jasmine Anderson
+* @version 1.0
+*/
 public abstract class Modifier<T,K> {
 
     public AccessModifier accessModifier;
     public NonAccessModifier nonAccessModifier;
     public PrimitiveDataTypeModifier primitiveDataTypeModifier;
-    public NonPrimitiveDataTypeModifier nonPrimitiveDataTypeModifier;
-    public NonPrimitiveDataTypeModifier nonPrimitiveDataTypeModifierBrackets;
-    public String objectDataTypeModifier;
+    public T nonPrimitiveDataTypeModifier;
+    public K dataTypeModifierDiamond;
+    
+    public Modifier(
+        AccessModifier accessModifier,
+        NonAccessModifier nonAccessModifier,
+        PrimitiveDataTypeModifier primitiveDataTypeModifier
+    ) {
+        this.accessModifier = accessModifier;
+        this.nonAccessModifier = nonAccessModifier;
+        this.primitiveDataTypeModifier = primitiveDataTypeModifier;
+    };
 
-    public List<Field> modifiers() {
+    public Modifier(
+        AccessModifier accessModifier,
+        NonAccessModifier nonAccessModifier,
+        T nonPrimitiveDataTypeModifier,
+        K dataTypeModifierDiamond
+    ) {
+        this.accessModifier = accessModifier;
+        this.nonAccessModifier = nonAccessModifier;
+        this.nonPrimitiveDataTypeModifier = nonPrimitiveDataTypeModifier;
+        this.dataTypeModifierDiamond = dataTypeModifierDiamond;
+    };
+
+    /**
+     * List format of attribute value for Modifier class
+     * @return List of Modifier attribute values
+     * @see Class#getDeclaredFields() Class.getDeclaredFields()
+     * @see Arrays#asList(T...) Arrays.asList()
+     * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/List.html">List</a> 
+     */
+    public List<Field> fields() {
         return Arrays.asList(Modifier.class.getDeclaredFields());
     };
 
+    /**
+     * Checks if List contains any valid values
+     * @return True if List contains a valid value and false if all Null or empty String
+     * @see List#isEmpty() List.isEmpty()
+     * @see List#containsAll(Collection) List.containsAll()
+     */
     public Boolean isEmpty() {     
-        return (modifiers().isEmpty() || modifiers().containsAll(null));
+        return (fields().isEmpty() || fields().containsAll(null));
     };
     
-    public String nonPrimitiveDataTypeModifier() {
-        String formatted = nonPrimitiveDataTypeModifierBrackets == null ? String.format("%s", nonPrimitiveDataTypeModifier) : 
-        String.format("%s<%s>", nonPrimitiveDataTypeModifier, nonPrimitiveDataTypeModifierBrackets);
-        
-        return formatted;
-    };
-
-    public String modifier() {
-        // convert attributes to string
-        String modifiers = String.format("%s %s %s %s %s", accessModifier, nonAccessModifier,
-                primitiveDataTypeModifier, nonPrimitiveDataTypeModifier(), objectDataTypeModifier);
-
-        // convert string to arr and remove null
-        String[] arr = Arrays.stream(modifiers
-                .split(" "))
-                .filter(Objects::nonNull)
-                .toArray(String[]::new);
-
-        return Optional.ofNullable(String.join(" ", arr)) == null ? "" : String.join(" ", arr);
-    };
-    
+    /**
+    * Removes nulls and converts List<Fields> to a String 
+    * @return String of all attribute values filtered for nulls or empty string if all attribute values are null
+    * @see List#parallelStream() List.parallelStream()
+    * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html#filter-java.util.function.Predicate-">Stream.filter()</a>
+    * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html#map-java.util.function.Function-">Stream.map()</a>
+    * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html#collect-java.util.stream.Collector-">Stream.collect()</a>
+     */
     @Override
     public String toString() {
-        String modifier = this.isEmpty() ? "" : modifier();
-        return modifier;
+        List<String> arr = fields()
+            .parallelStream()
+            .filter(Objects::nonNull)
+            .map(object -> Objects.toString(object, null))
+            .collect(Collectors.toList());
+        
+        String str = String.join(" ", arr);
+
+        return str == null ? "" : str;
     };
 
     public abstract Boolean isValid();
 
-    public abstract Optional<T> accessType();
+    public abstract Object accessType();
 
-    public abstract Optional<K> dataType();
+    public abstract Object dataType();
 }
